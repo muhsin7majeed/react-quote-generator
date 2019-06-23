418ee4572b69235efe506e4d2f574b7d7b4baed9
Muhsin <muhsin7ak@gmail.com>
2019-06-23 09:54:25 +0530

    Updates

---
+++ precache-manifest.ec2ce4731744b7966cd996ab1de38cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8d5193f6b40345bfb57eb6b8610021",
+    "url": "/react-quote-generator/index.html"
+  },
+  {
+    "revision": "5f6f8793a5c16d460f6b",
+    "url": "/react-quote-generator/static/css/main.c62d4bed.chunk.css"
+  },
+  {
+    "revision": "c1187ae7b3d70c1a8010",
+    "url": "/react-quote-generator/static/js/2.dedde616.chunk.js"
+  },
+  {
+    "revision": "5f6f8793a5c16d460f6b",
+    "url": "/react-quote-generator/static/js/main.04f8e978.chunk.js"
+  },
+  {
+    "revision": "f2c802330a7e397a93fa",
+    "url": "/react-quote-generator/static/js/runtime~main.b9c71da4.js"
+  },
+  {
+    "revision": "abfce4ef7523be5695e9e2db06af6ff2",
+    "url": "/react-quote-generator/static/media/bg.abfce4ef.mp4"
+  }
+]);
